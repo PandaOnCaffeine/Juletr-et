747240050a9a5ec22262c55267eed9e1a40d3d11
PandaOnCaffeine <tuepvm@gmail.com>
2022-08-09 14:24:29 +0200

    .

--- Juletræet/Program.cs
+++ Juletræet/Program.cs
@@ -7,15 +7,12 @@
             //I define the star and spaces that i need for the first line
             string stars = "*";
             string spaces = "       ";
-            
-            //I make a int sequence for later use, to get the right order of color
-            int sequence = 0;
 
             //a for loop for the 8 lines
             for (int i = 0; i < 8; i++)
             {
                 //a if to check what line its on
-                if (i == 0 || i == 2 || i == 4 | i == 6 | i == 8) 
+                if (i %2 == 0) 
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine($"{spaces}{stars}{spaces}");
@@ -23,7 +20,6 @@
                 else
                 {
                     //a foreach loop to color every other star red
-                    sequence = 0;
                     Console.Write($"{spaces}");
                     int starvalue = 0;
                     foreach (char star in stars)
